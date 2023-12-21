57f661165854935d094fb018d74a5bb6777141aa
Purushotam Thakur <109227953+077bel-030purshottam@users.noreply.github.com>
2023-12-21 22:41:22 +0545

    Add files via upload

---
+++ helloworld/Program.cs
@@ -0,0 +1,16 @@
+﻿// using Internal;
+using System;
+namespace myproject
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int a = 10;
+            Console.WriteLine("The value of the a  is " + a);
+
+        }
+    }
+}
+
---
+++ integer/Program.cs
@@ -0,0 +1,18 @@
+﻿// using Internal;
+using System;
+namespace myproject
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            Console.WriteLine(a);
+            Console.WriteLine(a);
+            Console.WriteLine(a);
+            Console.WriteLine(a);
+            Console.WriteLine(a);
+            Console.WriteLine(a);
+        }
+    }
+}
