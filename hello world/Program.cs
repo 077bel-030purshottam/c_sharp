5190a512657a52191e093402777262efeeb6e21e
Purushotam Thakur <109227953+077bel-030purshottam@users.noreply.github.com>
2023-12-21 22:43:55 +0545

    Add files via upload

---
+++ hello world/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hello_world
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name ");
+            string name = Console.ReadLine();
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("Enter the value of the a");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
